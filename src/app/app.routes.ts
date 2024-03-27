9c7fa82392e78921ac5bd4f3b2b0448d3fff5759
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-03-27 14:04:20 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'principal',
+    pathMatch: 'full'
   },
+
   {
     path: 'principal',
     loadComponent: () => import('./pages/principal/principal.page').then( m => m.PrincipalPage)
