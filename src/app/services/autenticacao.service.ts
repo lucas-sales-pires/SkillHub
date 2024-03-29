f81002bcb8c7406dd51cf05ec2cdbbe6e2fb9029
lucas-sales-pires <lucas2013sales@hotmail.com>
2024-03-29 11:50:37 -0300

    .

--- src/app/services/autenticacao.service.ts
+++ src/app/services/autenticacao.service.ts
@@ -1,23 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  constructor(private afAuth: AngularFireAuth) {}
+export class AutenticacaoService {
 
-  register(email: string, password: string): Promise<auth.UserCredential> {
-    return this.afAuth.createUserWithEmailAndPassword(email, password);
-  }
-
-
-  
-  login(email: string, password: string): Promise<auth.UserCredential> {
-    return this.afAuth.signInWithEmailAndPassword(email, password);
-  }
-
-  logout(): Promise<void> {
-    return this.afAuth.signOut();
-  }
+  constructor() { }
 }
